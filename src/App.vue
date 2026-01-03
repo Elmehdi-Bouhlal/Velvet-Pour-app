cfd66be4a4b8251ae4b4ac23e911bb024984e893
Elmehdi Bouhlal <bouhlalmahdi8@gmail.com>
2026-01-03 18:04:22 +0100

    injection : add header componenet

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script setup>
+import gsap from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+import { SplitText } from "gsap/SplitText";
+import Navbar from "./components/Navbar.vue";
+
+gsap.registerPlugin(ScrollTrigger, SplitText);
+</script>
+
+<template>
+    <main>
+        <Navbar />
+    </main>
+</template>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,43 @@
+<script setup lang="ts">
+import { onMounted } from "vue";
+import { navLinks } from "../../constants";
+import gsap from "gsap";
+
+onMounted(() => {
+    const navTween = gsap.timeline({
+        scrollTrigger: {
+            trigger: "nav",
+            start: "bottom top",
+        },
+    });
+
+    navTween.fromTo(
+        "nav",
+        {
+            backgroundColor: "transparent",
+        },
+        {
+            duration: 1,
+            backgroundColor: "#0000050",
+            ease: "power1.inOut",
+            backgroundFilter: "blur(10px)",
+        },
+    );
+});
+</script>
+
+<template>
+    <nav>
+        <div>
+            <a href="#home" class="flex items-center gap-2">
+                <img src="/images/logo.png" alt="Logo" />
+                <p>Velvet Pour</p>
+            </a>
+            <ul>
+                <li v-for="link in navLinks" :key="link.id">
+                    <a :href="`#${link.id}`">{{ link.title }}</a>
+                </li>
+            </ul>
+        </div>
+    </nav>
+</template>
