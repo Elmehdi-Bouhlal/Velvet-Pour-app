1e04848708e2ebfba4191ae36fbb29c51d045067
Elmehdi Bouhlal <bouhlalmahdi8@gmail.com>
2026-01-09 22:59:24 +0100

    add some modifcation at the navbar section

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -4,41 +4,42 @@ import { navLinks } from "../../constants";
 import gsap from "gsap";
 
 onMounted(() => {
-    // Initialize GSAP animations
-    const navTween = gsap.timeline({
-        scrollTrigger: {
-            trigger: "nav",
-            start: "bottom top",
-        },
-    });
+  // Initialize GSAP animations
+  // nav bar section
+  const navTween = gsap.timeline({
+    scrollTrigger: {
+      trigger: "nav",
+      start: "bottom top",
+    },
+  });
 
-    navTween.fromTo(
-        "nav",
-        {
-            backgroundColor: "transparent",
-        },
-        {
-            duration: 1,
-            backgroundColor: "#00000050",
-            ease: "power1.inOut",
-            backgroundFilter: "blur(10px)",
-        },
-    );
+  navTween.fromTo(
+    "nav",
+    {
+      backgroundColor: "transparent",
+    },
+    {
+      duration: 1,
+      backgroundColor: "#00000050",
+      ease: "power1.inOut",
+      backgroundFilter: "blur(10px)",
+    },
+  );
 });
 </script>
 
 <template>
-    <nav>
-        <div>
-            <a href="#home" class="flex items-center gap-2">
-                <img src="/images/logo.png" alt="Logo" />
-                <p>Velvet Pour</p>
-            </a>
-            <ul>
-                <li v-for="link in navLinks" :key="link.id">
-                    <a :href="`#${link.id}`">{{ link.title }}</a>
-                </li>
-            </ul>
-        </div>
-    </nav>
+  <nav>
+    <div>
+      <a href="#home" class="flex items-center gap-2">
+        <img src="/images/logo.png" alt="Logo" />
+        <p>Velvet Pour</p>
+      </a>
+      <ul>
+        <li v-for="link in navLinks" :key="link.id">
+          <a :href="`#${link.id}`">{{ link.title }}</a>
+        </li>
+      </ul>
+    </div>
+  </nav>
 </template>
